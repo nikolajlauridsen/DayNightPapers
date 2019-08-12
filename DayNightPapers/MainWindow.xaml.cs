91889495aa69599ae55c3027e0fc2405836e643f
Molex <simplesleeptimer@gmail.com>
2019-08-12 12:33:57 +0200

    Add project files.

---
+++ DayNightPapers/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+using WallpaperLib;
+using Path = System.IO.Path;
+
+
+namespace DayNightPapers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WallpaperStore store = new WallpaperStore();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (store.DayPaper != null) DayPaperLocation.Content = Path.GetFileName(store.DayPaper);
+            else DayPaperLocation.Content = "Please select a wallpaper";
+            if (store.NightPaper != null) NightPaperLocation.Content = Path.GetFileName(store.NightPaper);
+            else NightPaperLocation.Content = "Please select a wallpaper";
+        }
+
+        private void DayPickerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string paperPath = selectImage();
+            if (paperPath != null)
+            {
+                store.DayPaper = paperPath;
+                DayPaperLocation.Content = Path.GetFileName(store.DayPaper); ;
+            }
+            
+        }
+
+        private void NightPickerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string paperPath = selectImage();
+            if (paperPath != null)
+            {
+                store.NightPaper = paperPath;
+                NightPaperLocation.Content = Path.GetFileName(store.NightPaper);
+            }
+        }
+
+        private string selectImage()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".png";
+            dialog.Filter = "PNG image files (*.png)|*.png";
+            bool? result = dialog.ShowDialog();
+
+            if (result == true) {
+                return dialog.FileName;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ WallpaperLib/WallpaperStore.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WallpaperLib.Properties;
+
+namespace WallpaperLib
+{
+    public class WallpaperStore
+    {
+        private static string _cacheName = @"PaperCache";
+
+        public string CacheFolder
+        {
+            get
+            {
+                if (Directory.Exists(_cacheName)) return _cacheName;
+                Directory.CreateDirectory(_cacheName);
+                return _cacheName;
+            }
+        }
+
+        public string DayPaper
+        {
+            get
+            {
+                string dayPaperPath = GetAbsolutePath(Settings.Default.DayPaperName);
+                if (File.Exists(dayPaperPath)) return dayPaperPath;
+                else return null;
+
+            }
+            set
+            {
+                string newFileName = storePaper(Settings.Default.DayPaperName, value, "_day");
+                Settings.Default.DayPaperName = newFileName;
+                Settings.Default.Save();
+
+            }
+        }
+
+        public string NightPaper
+        {
+            get
+            {
+                string nightPaperPath = GetAbsolutePath(Settings.Default.NightPaperName);
+                if (File.Exists(nightPaperPath)) return nightPaperPath;
+                else return null;
+            }
+            set
+            {
+                string newFileName = storePaper(Settings.Default.NightPaperName, value, "_night");
+                Settings.Default.NightPaperName = newFileName;
+                Settings.Default.Save();
+            }
+        }
+
+        private string storePaper(string oldFileName, string newPath, string fileSuffix)
+        {
+            // Delete old cached wallpaper if it exists
+            string oldPaperPath = Path.Combine(Environment.CurrentDirectory, CacheFolder, oldFileName);
+            if (File.Exists(oldPaperPath)) File.Delete(oldPaperPath);
+
+            // Copy new wallpaper
+            string newFileName = Path.GetFileNameWithoutExtension(newPath) + fileSuffix + Path.GetExtension(newPath);
+            File.Copy(newPath, Path.Combine(Environment.CurrentDirectory, CacheFolder, newFileName));
+            return newFileName;
+        }
+
+        private string GetAbsolutePath(string fileName)
+        {
+            return Path.Combine(Environment.CurrentDirectory, CacheFolder, fileName);
+        }
+    }
+}
